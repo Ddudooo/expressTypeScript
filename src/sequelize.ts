8b37b31169afdd863145008b2a8a21d1c38af9e8
Jungdoo <ddudoo95@gmail.com>
2019-12-09 18:40:56 +0900

    Update github workflows

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -20,6 +20,7 @@ try {
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
         host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
         dialect: process.env.DB_DIALECT,
         operatorAliases: false,
         timezone: process.env.DB_TIMEZONE,
