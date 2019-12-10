fca17ac4bd9de47a8f1a5942d93d135e13e283a7
Jungdoo <ddudoo95@gmail.com>
2019-12-10 12:43:06 +0900

    Update test

--- src/test/express.test.ts
+++ src/test/express.test.ts
@@ -1,10 +1,9 @@
 import { expect } from "chai";
-//import httpMocks from "node-mocks-http";
 //const sinon = require("sinon");
 import sinon from "sinon";
 import request from "supertest";
 import cheerio from "cheerio";
-import cookieParser from "cookie-parser";
+
 import path from "path";
 import dotenv from "dotenv";
 dotenv.config({ path: path.resolve(__dirname, ".env") });
@@ -12,41 +11,10 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 import app from "../app";
 import { Member, LoginLog, Token } from "../models/member";
 
-function pathCreate(
-    path: string,
-    method: string,
-    result: { [key: string]: any | Function }
-) {
-    return { path: path, method: method, result: result };
-}
-
-function pathTest(
-    req: request.SuperTest<request.Test>,
-    a: { [key: string]: any }
-) {
-    const aMETHOD = a.method.toUpperCase();
-    if (aMETHOD === "GET") {
-        return it(`URL PATH : ${a.path}`, async () => {
-            if (a.result instanceof Function) {
-                await req.get(a.path).expect(a.result);
-            } else {
-                await req.get(a.path).expect(a.result.status || 200);
-            }
-        });
-    } else if (aMETHOD === "POST") {
-        return it(`URL PATH : ${a.path}`, async () => {
-            if (a.result instanceof Function) {
-                await req.post(a.path).expect(a.result);
-            } else {
-                await req.post(a.path).expect(a.result.status || 200);
-            }
-        });
-    }
-}
-
 describe("Express test", () => {
     const req = request(app);
     beforeEach(() => {
+        //로그인 테스트용
         const result: Member[] = [];
         const fakeUser = new Member({
             idx: 0,
@@ -71,16 +39,27 @@ describe("Express test", () => {
         sinon.restore();
     });
     describe("UnAuthorized Path", () => {
-        const path = [];
-        path.push(pathCreate("/", "get", { status: 200 }));
-        path.push(pathCreate("/signin", "get", { status: 200 }));
-        path.push(pathCreate("/signup", "get", { status: 200 }));
+        const path = [
+            "/",
+            "/signin",
+            "/signup",
+            "/webapi/stream",
+            "/webapi/media"
+        ];
         for (const a of path) {
-            pathTest(req, a);
+            it(`URL PATH : ${a}`, async () => {
+                await req.get(a).expect(200);
+            });
         }
     });
     describe("Authorize Require Path", () => {
-        const path = [];
+        const path = [
+            "/logout",
+            "/member/info",
+            "/tl",
+            "/navi",
+            "/webapi/socket"
+        ];
         const expectFunc = (res: request.Response) => {
             expect(res.status).to.be.equal(401);
             const $ = cheerio.load(res.text);
@@ -89,12 +68,10 @@ describe("Express test", () => {
                 .text();
             expect(bodyTitle).to.equal("SIGN IN");
         };
-        path.push(pathCreate("/logout", "get", expectFunc));
-        path.push(pathCreate("/member/info", "get", expectFunc));
-        path.push(pathCreate("/tl", "get", expectFunc));
-        path.push(pathCreate("/webapi/socket", "get", expectFunc));
         for (const a of path) {
-            pathTest(req, a);
+            it(`URL PATH : ${a}`, async () => {
+                await req.get(a).expect(expectFunc);
+            });
         }
     });
     describe("Login Member", () => {
@@ -115,8 +92,10 @@ describe("Express test", () => {
             const cookie = res.header["set-cookie"];
             expect(cookie.join(",")).to.have.string("test.sign");
         });
+    });
 
-        it("URL PATH : /member/info", async () => {
+    describe("ACCESS TOKEN TEST", () => {
+        beforeEach(() => {
             const tokenUtils = require("../utils/tokenUtils");
             sinon.stub(tokenUtils, "verifyJWT").callsFake(() => "accesstoken");
             sinon
@@ -129,17 +108,87 @@ describe("Express test", () => {
                     password: "1234"
                 })
             );
-            const res = await req
-                .get("/member/info")
-                .set("Cookie", "test.sign=acc;test.refresh=ref")
-                .expect(200);
-
+        });
+        afterEach(() => {
+            sinon.restore();
+        });
+        const path = ["/member/info", "/tl", "/navi", "/webapi/socket"];
+        const expectFunc = (res: request.Response) => {
+            expect(res.status).to.be.equal(200);
             const $ = cheerio.load(res.text);
             const bodyTitle = $("h2")
                 .first()
                 .text();
             expect(bodyTitle).to.not.equal("SIGN IN");
             sinon.restore();
+        };
+        for (const a of path) {
+            it(`URL PATH : ${a}`, async () => {
+                await req
+                    .get(a)
+                    .set("Cookie", "test.sign=acc;test.refresh=ref")
+                    .expect(expectFunc);
+            });
+        }
+        it(`URL PATH : /logout`, async () => {
+            await req
+                .get("/logout")
+                .set("Cookie", "test.sign=acc;test.refresh=ref")
+                .expect(302);
+        });
+    });
+
+    describe("REFRESH TOKEN TEST", () => {
+        beforeEach(() => {
+            const tokenUtils = require("../utils/tokenUtils");
+
+            sinon.stub(tokenUtils, "verifyJWT").callsFake(token => {
+                if (token === "acc") {
+                    throw new Error();
+                } else {
+                    return "refresh";
+                }
+            });
+            sinon
+                .stub(tokenUtils, "refreshToken")
+                .resolves(new Token({ memberIdx: 0 }));
+            sinon
+                .stub(Token, "findAll")
+                .resolves([new Token({ memberIdx: 0 })]);
+            sinon.stub(Member, "findByPk").resolves(
+                new Member({
+                    idx: 0,
+                    userId: "test",
+                    password: "1234"
+                })
+            );
+        });
+        afterEach(() => {
+            sinon.restore();
+        });
+        const path = ["/member/info", "/tl", "/navi", "/webapi/socket"];
+        const expectFunc = (res: request.Response) => {
+            expect(res.status).to.be.equal(302);
+            const $ = cheerio.load(res.text);
+            const bodyTitle = $("h2")
+                .first()
+                .text();
+            expect(bodyTitle).to.not.equal("SIGN IN");
+            sinon.restore();
+        };
+        for (const a of path) {
+            it(`URL PATH : ${a}`, async () => {
+                await req
+                    .get(a)
+                    .set("Cookie", "test.sign=acc;test.refresh=ref")
+                    .expect(expectFunc);
+            });
+        }
+        it(`URL PATH : /logout`, async () => {
+            await req
+                .get("/logout")
+                .set("Cookie", "test.sign=acc;test.refresh=ref")
+                .expect(302);
         });
     });
 });
